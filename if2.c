741fe134494c26ce13545b5e21342a89a4f2a1d9
Kurochan <kurochan@example.com>
2011-05-29 00:04:26 +0900

    if文でブロックを使うサンプル

---
+++ if2.c
@@ -0,0 +1,22 @@
+/*
+ * if文のサンプル
+ * {}でくくってブロックとして扱う
+ */
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+    int b = 1;
+
+    //ブロックを使って、if文の範囲を指定していることに注目
+    if(a>b) {
+        printf("aはbよりも大きいです\n");
+        b = 3;
+    }
+
+    if(a==b)
+        printf("aとbは等しいです\n");
+
+    return 0;
+}
