317fa5e20c354359d95f8876ee0232237fb8c6e8
Kurochan <kurochan@example.com>
2011-05-28 23:18:06 +0900

    printfの使い方

---
+++ printf.c
@@ -0,0 +1,14 @@
+/*
+ * printfの使い方のサンプル
+ */
+#include <sidio.h>
+
+int main()
+{
+    int number;
+    //文字列を出力
+    printf("Hello!\n");
+    //文字列と変数を出力
+    printf("number = %d\n",number);
+    return 0;
+}
