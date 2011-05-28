5cd74d01a68ce10d101b209af1db715fea0ff07c
Kurochan <kurochan@example.com>
2011-05-28 23:21:09 +0900

    scanf関数の使い方

---
+++ scanf.c
@@ -0,0 +1,15 @@
+/*
+ * 数字を入力して、入力された数字を
+ * そのまま出力するサンプル
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int value=0;
+    printf("Input a value:");
+    scanf("%d",&value);
+    printf("value = %d",value);
+    return 0;
+}
