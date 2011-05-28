61135997e21502c5298569114969b1143614c94a
Kurochan <kurochan@example.com>
2011-05-28 23:21:36 +0900

    for文の使い方(1)

---
+++ for1.c
@@ -0,0 +1,16 @@
+/*
+ * for文のサンプル
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int i;  //カウント用の変数
+
+    //for(初期化文;継続条件;インクリメント文)
+    for(i=0; i<10; i++) {
+        printf("i = %d¥n",i);
+    }
+
+    return (0);
+}
